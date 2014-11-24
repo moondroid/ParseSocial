4d595042e949975d33b0e3415de605af95c781f2
Marco Granatiero <marco.granatiero@3wlab.it>
2014-11-24 16:29:10 +0100

    add custom post adapter

--- sociallib/src/main/java/it/moondroid/sociallib/fragments/AllPostsFragment.java
+++ sociallib/src/main/java/it/moondroid/sociallib/fragments/AllPostsFragment.java
@@ -1,5 +1,6 @@
 package it.moondroid.sociallib.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,38 +11,30 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import it.moondroid.sociallib.R;
+import it.moondroid.sociallib.it.moondroid.sociallib.adapters.PostsQueryAdapter;
 
 /**
  * Created by marco.granatiero on 24/11/2014.
  */
 public class AllPostsFragment extends Fragment implements AdapterView.OnItemClickListener {
 
-    private ArrayList<ParseObject> objects = new ArrayList<ParseObject>();
-    private ArrayList<String> titles = new ArrayList<String>();
-    private ArrayAdapter<String> adapter;
-
+    private PostsQueryAdapter adapter;
 
     public AllPostsFragment() {
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        if (savedInstanceState == null){
-            update();
-        }
     }
 
 
@@ -50,12 +43,11 @@ public class AllPostsFragment extends Fragment implements AdapterView.OnItemClic
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_all_posts, container, false);
 
-        if(savedInstanceState!=null){
-            titles = (ArrayList<String>) savedInstanceState.getSerializable("titles");
-        }
-
         ListView objectListView = (ListView)fragmentView.findViewById(R.id.objects_list);
-        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, titles);
+        adapter = new PostsQueryAdapter(getActivity());
+        //adapter.setTextKey("text");
+        //adapter.setImageKey("photo");
+
         objectListView.setAdapter(adapter);
         objectListView.setEmptyView(fragmentView.findViewById(R.id.empty_list));
 
@@ -69,36 +61,10 @@ public class AllPostsFragment extends Fragment implements AdapterView.OnItemClic
 
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        outState.putSerializable("titles", titles);
-        super.onSaveInstanceState(outState);
-    }
 
     public void update(){
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Post");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> parseObjects, ParseException e) {
-                if (e == null) {
-                    // Success!
-                    Toast.makeText(getActivity(), "found " + parseObjects.size() + " objects", Toast.LENGTH_SHORT).show();
-                    objects.clear();
-                    titles.clear();
-                    for(ParseObject object : parseObjects){
-                        Log.d("AllPostsFragment.findInBackground.done", "id: " + object.getObjectId() + " text: " + object.getString("text"));
-                        titles.add(object.getString("text"));
-                        objects.add(object);
-                    }
-                    adapter.notifyDataSetChanged();
-
-                }else {
-                    // Failure!
-                    Log.e("AllPostsFragment.findInBackground.done", "error: " + e.getLocalizedMessage());
-                    Toast.makeText(getActivity(), "error", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
+        adapter.loadObjects();
     }
+
+
 }
---
+++ sociallib/src/main/java/it/moondroid/sociallib/it/moondroid/sociallib/adapters/PostsQueryAdapter.java
@@ -0,0 +1,58 @@
+package it.moondroid.sociallib.it.moondroid.sociallib.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+
+import it.moondroid.sociallib.R;
+
+/**
+ * Created by marco.granatiero on 24/11/2014.
+ */
+public class PostsQueryAdapter extends ParseQueryAdapter {
+
+    public PostsQueryAdapter(Context context) {
+        //super(context, "Post");
+        super(context, new ParseQueryAdapter.QueryFactory<ParseObject>() {
+            public ParseQuery create() {
+                ParseQuery query = new ParseQuery("Post");
+                query.include("from"); //retrieve user also
+                //query.whereEqualTo("highPri", true);
+                return query;
+            }
+        });
+
+    }
+
+
+    @Override
+    public View getItemView(ParseObject object, View v, ViewGroup parent) {
+        if (v == null) {
+            v = View.inflate(getContext(), R.layout.item_post, null);
+        }
+
+        // Take advantage of ParseQueryAdapter's getItemView logic for
+        // populating the main TextView/ImageView.
+        // The IDs in your custom layout must match what ParseQueryAdapter expects
+        // if it will be populating a TextView or ImageView for you.
+        //super.getItemView(object, v, parent);
+
+        // Do additional configuration before returning the View.
+        TextView descriptionView = (TextView) v.findViewById(R.id.post_content);
+        descriptionView.setText(object.getString("text"));
+
+        TextView dateView = (TextView) v.findViewById(R.id.post_date);
+        android.text.format.DateFormat df = new android.text.format.DateFormat();
+        dateView.setText(df.format("dd MMMM - hh:mm", object.getDate("date")));
+
+        TextView userView = (TextView) v.findViewById(R.id.user_name);
+        userView.setText(object.getParseUser("from").getUsername());
+
+        return v;
+    }
+}
