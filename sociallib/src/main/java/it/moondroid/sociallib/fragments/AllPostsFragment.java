6d70967e26ddf8bf68aa4c6c5c251fa16c721f53
Marco Granatiero <marco.granatiero@3wlab.it>
2014-12-02 11:55:33 +0100

    add DefaultItemAnimator

--- sociallib/src/main/java/it/moondroid/sociallib/fragments/AllPostsFragment.java
+++ sociallib/src/main/java/it/moondroid/sociallib/fragments/AllPostsFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -63,6 +64,7 @@ public class AllPostsFragment extends Fragment implements OnRecyclerViewItemClic
         // elements are laid out.
         mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
         mAdapter = new PostsRecyclerViewAdapter(getActivity());
         mRecyclerView.setAdapter(mAdapter);
